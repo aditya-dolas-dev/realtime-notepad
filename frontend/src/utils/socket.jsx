16065f6adedea2fdc16984aa3c2a1b505724300a
Aditya Dolas <adityadolas007@gmail.com>
2025-07-15 15:55:58 +0530

    Update socket.jsx

--- frontend/src/utils/socket.jsx
+++ frontend/src/utils/socket.jsx
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://real-time-notepad-7a5c.onrender.com", {
+export const socket = io("http://localhost:3000", {
   transports: ["websocket"],
 });
